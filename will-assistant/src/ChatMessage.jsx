a5b7b1350d7365004c2598d91812420a77321b9c
AntonRize <Antonrize@gmail.com>
2025-06-29 19:40:51 +1000

    Create ChatMessage.jsx

---
+++ will-assistant/src/ChatMessage.jsx
@@ -0,0 +1,17 @@
+import ReactMarkdown from 'react-markdown';
+import remarkMath      from 'remark-math';
+import rehypeKatex     from 'rehype-katex';
+import 'katex/dist/katex.min.css';
+
+export default function ChatMessage({ role, text }) {
+  return (
+    <div className={`msg ${role}`}>
+      <ReactMarkdown
+        remarkPlugins={[remarkMath]}
+        rehypePlugins={[rehypeKatex]}
+      >
+        {text}
+      </ReactMarkdown>
+    </div>
+  );
+}
