f8160fc08f11544293c5747f1d94f03786dcb5aa
AntonRize <Antonrize@gmail.com>
2026-04-13 19:45:00 +1000

    SEO UP

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './_layouts/**/*.html',
+    './_includes/**/*.html',
+    './*.md',
+    './*.html',
+    './decoder/**/*.html',
+    './assistant/**/*.html',
+    './relativistic-foundations.md',
+    './_parts/**/*.{html,md}',
+    './_results/**/*.{html,md}',
+    './_discussions/**/*.{html,md}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
